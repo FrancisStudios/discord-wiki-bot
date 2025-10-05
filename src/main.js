96c5e0dee39d5fd807abfe469c8578615fd3ded1
Francis <vlfmap@gmail.com>
2025-10-05 10:42:20 +0200

    basic bot setup

--- src/main.js
+++ src/main.js
@@ -4,3 +4,29 @@
  * github: https://github.com/francisstudios
  * ©2025 Francis Studios Softwares by L.
 */
+
+import { Client, GatewayIntentBits } from 'discord.js';
+import { config } from 'dotenv';
+
+/* Bot Intents And Permissions Section Should be Here */
+const _botClientIntents = {
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent
+    ]
+};
+
+
+/* Bot Authentications Should All be Here In this Section */
+const DiscordBotClient = new Client(_botClientIntents);
+config();
+DiscordBotClient.login(process.env.DISCORD_WIKI_BOT_API_KEY);
+
+
+/* Control Flow And Program Blueprint Should Live Over Here */
+DiscordBotClient.on('messageCreate', (msg) => {
+
+});
