98dd1801111503831ac9a0743e82ac76cd9fe571
Francis <vlfmap@gmail.com>
2025-10-07 07:20:35 +0200

    sgn

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,3 +1,10 @@
+/**
+ *  ------ [ Property of Francis Studios ] ------
+ * ==============================================
+ * github: https://github.com/francisstudios
+ * ©2025 Francis Studios Softwares by L.
+*/
+
 const LOG_MESSAGES = {
     CLIENT_READY : '🟢 Discord Wiki Bot Client Ready'
 }
--- src/ENUM.js
+++ src/ENUM.js
@@ -1,3 +1,10 @@
+/**
+ *  ------ [ Property of Francis Studios ] ------
+ * ==============================================
+ * github: https://github.com/francisstudios
+ * ©2025 Francis Studios Softwares by L.
+*/
+
 const DISCORD_EVENTS = {
     MESSAGE_CREATE: 'messageCreate',
     CLIENT_READY: 'clientReady'
